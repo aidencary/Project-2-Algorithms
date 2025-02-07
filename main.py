eb836cdf21a42d83500cb550d0fe8dff95b8c69c
Aiden Cary <111547296+aidencary@users.noreply.github.com>
2025-02-07 11:46:51 -0600

    Added file imports

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # Testing the efficiency of different sort 
 # on a randomly generated array of numbers based on the size from user input
 
+import sys, os
+
+# Add the sorting_algorithms directory to the system path
+sys.path.append(os.path.join(os.path.dirname(__file__), "Sorts"))
+
+import random, time
+import bubble_sort, merge_sort
+
 def main():
     print("No implementation.")
 
